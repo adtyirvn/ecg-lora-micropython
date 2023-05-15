d5e18a51e8dadb7000637568bc45065dbb980ae1
Aditya Irvan <adityairvanku@gmail.com>
2023-05-15 10:37:47 +0700

    fix bugs
    work encryption
    
    fix

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import config
 import utime
 import ujson as json
+import struct
 
 # IMPORT PACKAGE
 from src import wifi_controller
@@ -20,18 +21,14 @@ ssid = config.WIFI_SSID
 password = config.WIFI_PASSWORD
 led_pin = config.LED_PIN
 dht_pin = config.DHT_PIN
-key = config.ENCRYPT_KEY
-nonce_g = config.ENCRYPT_NONCE
 
 delay_ms = 2000
 
 # LoRa
 controller = config_lora.Controller()
-lora = controller.add_transceiver(sx127x.SX127x(name = 'LoRa'),
-                                      pin_id_ss = config_lora.Controller.PIN_ID_FOR_LORA_SS,
-                                      pin_id_RxDone = config_lora.Controller.PIN_ID_FOR_LORA_DIO0)
-    
-node_name = config_lora.NODE_NAME
+lora = controller.add_transceiver(sx127x.SX127x(name='LoRa'),
+                                  pin_id_ss=config_lora.Controller.PIN_ID_FOR_LORA_SS,
+                                  pin_id_RxDone=config_lora.Controller.PIN_ID_FOR_LORA_DIO0)
 
 # WiFi
 wifi = wifi_controller.WiFiConnection(ssid, password, led_pin)
@@ -46,99 +43,124 @@ oled = display_ssd1306_i2c.Display(i2c)
 
 # ENKRIPSI
 asc = ascon.Ascon()
-# SETUP
-oled.fill(0)
-oled.text('starting...', 0, 0)
-oled.show()
-status, ip = wifi.connect()
-oled.fill(0)
-oled.text(status, 0, 0)
-oled.text(ip, 0, 10)
-oled.show()
-sleep(1)
-rtc.set_from_internet()
-date, time = rtc.get_formatted_datetime()
-oled.fill(0)
-oled.text(date, 0, 0)
-oled.text(time, 0, 10)
-oled.show()
-sleep(1)
-status = wifi.disconnect()
-oled.fill(0)
-oled.text(status, 0, 0)
-oled.show()
-sleep(1)
+master_node = "raspi"
+node_one = "esp32"
+
+key = config.ENCRYPT_KEY
+nonce_g = config.ENCRYPT_NONCE
+
+node_name = config_lora.NODE_NAME
+
 
 def main():
-    # print(nonce_g)
-    previous_time = utime.ticks_ms()
-    while True:
-                try:
-                    current_time = utime.ticks_ms()
-                    elapsed_time = current_time - previous_time
-                    if elapsed_time >= delay_ms:
-                        send_callback(lora, rtc, dht, node_name, oled, asc, key, nonce_g)
-                        previous_time = current_time
-                         
-                except Exception as e:
-                    print(f"Error message: {e}")
-                    oled.fill(0)
-                    oled.text('error occured', 0, 0)
-                    oled.show()
-                    sleep(1)
-                except KeyboardInterrupt:
-                    oled.clear()
-                    print("Exit")
-                    break
+    try:
+        show_on_oled(oled, ["starting..."])
 
-def encryption(ascon, message, key, nonce, mode="ECB"):
-    print(f"key: {binascii.hexlify(key)} len: {len(key)}")
-    print(f"nonce: {binascii.hexlify(nonce)} len: {len(key)}")
-    ciphertext = ascon.ascon_encrypt(key, nonce,associateddata="", plaintext=message,  variant="Ascon-128") 
-    if mode == "CBC":
+        status, ip = wifi.connect()
+        show_on_oled(oled, [status, ip], delay=1)
+
+        rtc.set_from_internet()
+        date = rtc.get_formatted_date()
+        time = rtc.get_formatted_time()
+        show_on_oled(oled, [date, time], delay=1)
+
+        status = wifi.disconnect()
+        show_on_oled(oled, [status], delay=1)
+
+        show_on_oled(oled, ["Ready..."], delay=1)
+        print("Ready...")
+
+        while True:
+            try:
+                on_receive(lora, rtc, dht, node_name, oled, asc, key, nonce_g)
+            except Exception as e:
+                print(e)
+    except KeyboardInterrupt:
+        oled.clear()
+        show_on_oled(oled, ["goodbye..."])
+        sleep(5)
+        oled.clear()
+
+
+def show_on_oled(oled, items, delay=0):
+    oled.fill(0)
+    for i, text in enumerate(items):
+        oled.text(text, 0, 10 * i)
+    oled.show()
+    sleep(delay)
+
+
+def on_receive(lora, rtc_service, dht_service, name, oled, enc, key, nonce):
+    if lora.receivedPacket():
         global nonce_g
-        nonce_g = ciphertext[:16]
-    return ciphertext  
+        payload = lora.read_payload()
+        if not len(payload):
+            return
+        pay_json = payload.decode("utf-8")
+        pay_dict = json.loads(pay_json)
+        print(f"***Received packet***\n{pay_dict}")
+        rst = pay_dict.get("rst")
+        recipient = pay_dict["to"]
+        sender = pay_dict["id"]
+        if recipient != node_one and recipient != master_node:
+            return
+        if sender == master_node:
+            nonce_gg = send_callback(lora, rtc_service, dht_service, name,
+                                     oled, enc, key, config.ENCRYPT_NONCE if bool(rst) else nonce)
+            nonce_g = nonce_gg
 
-def get_json_data(dht_service, rtc_service, id):
-    current_datetime = rtc_service.get_datetime()
-    temperature, humidity = dht_service.read()
-    data = {
-            "id": id,
-            "t": temperature,
-            "h": humidity,
-            "tsp": current_datetime
-        }
-    json_data = json.dumps(data)
-    return json_data
 
 def send_callback(lora, rtc_service, dht_service, name, oled, enc, key, nonce):
     payload = get_json_data(dht_service, rtc_service, name)
-    ciphertext = encryption(enc, payload.encode('utf-8'), key, nonce, "CBC")
-    cipher_hex = binascii.hexlify(ciphertext)
-    print(f"***Sending packet***\n{cipher_hex}\nlen: {len(cipher_hex)}")
-    lora.println(cipher_hex)
+    ciphertext, new_nonce = encryption(
+        enc, payload.encode("utf-8"), key, nonce, "CBC")
+    print(
+        f"***Sending packet***\n{payload.encode('utf-8')} len: {len(payload.encode('utf-8'))}")
+    lora.println(ciphertext)
     message = json.loads(payload)
+    print(message)
     show_info(oled, message)
+    return new_nonce
+
+
+def encryption(ascon, message, key, nonce, mode="ECB"):
+    print(f"key: {key} len: {len(key)}")
+    print(f"nonce: {nonce} len: {len(nonce)}")
+
+    ciphertext = ascon.ascon_encrypt(
+        key, nonce, associateddata=b"", plaintext=message,  variant="Ascon-128")
+    if mode == "CBC":
+        new_nonce = ciphertext[:16]
+        print(f"nw: {new_nonce} len: {len(new_nonce)}")
+    return ciphertext, new_nonce
+
+
+def get_json_data(dht_service, rtc_service, id):
+    dt = rtc_service.get_datetime()
+    # iso_time = "{:04}-{:02}-{:02}T{:02}:{:02}:{:02}".format(dt[0], dt[1], dt[2], dt[4], dt[5], dt[6])
+    temperature, humidity = dht_service.read()
+    data = {
+        "id": node_one,
+        "to": master_node,
+        "t": temperature,
+        "h": humidity,
+        "tsp": dt
+    }
+    json_data = json.dumps(data)
+    return json_data
+
 
 def show_info(oled, message):
     date = f"{get_formatted_date(message['tsp'])}"
     time = f"{get_formatted_time(message['tsp'])}"
     t = f"T: {message['t']}C"
     h = f"H: {message['h']}%"
-    # oled.show_text(text = date, clear_first = False)
-    # oled.show_text(text = time, clear_first = False, x=0, y=10)  
-    # oled.show_text(text = t, clear_first = False, x=0, y=20)  
-    # oled.show_text(text = h, clear_first = False, x=0, y=30)
-    oled.fill(0)
-    oled.text(date, 0, 0) 
-    oled.text(time, 0, 10) 
-    oled.text(t, 0, 20) 
-    oled.text(h, 0, 30) 
-    oled.show()
-     
+    show_on_oled(oled, [date, time, t, h])
+
+
 def get_formatted_date(date_tuple):
     return f"{date_tuple[0]:04d}-{date_tuple[1]:02d}-{date_tuple[2]:02d}"
+
 
 def get_formatted_time(time_tuple):
     return f"{time_tuple[4]:02d}:{time_tuple[5]:02d}:{time_tuple[6]:02d}"
--- src/rtc_controller.py
+++ src/rtc_controller.py
@@ -1,25 +1,26 @@
 import machine
 import ntptime
 
+
 class RTCController:
     def __init__(self):
         self.rtc = machine.RTC()
-    
+
     def set_from_internet(self):
         try:
             ntptime.settime()
             print("RTC set from internet time.")
         except Exception as e:
             print("Error setting RTC from internet:", e)
-    
-    def get_formatted_datetime(self):
+
+    def get_formatted_date(self):
         datetime_tuple = self.rtc.datetime()
-        formatted_date = "{:04d}-{:02d}-{:02d}".format(
-        datetime_tuple[0], datetime_tuple[1], datetime_tuple[2])
-        formatted_time = "{:02d}:{:02d}:{:02d}".format(
-        datetime_tuple[4], datetime_tuple[5], datetime_tuple[6])
-        print(f"{formatted_date} {formatted_time}")
-        return formatted_date, formatted_time
-    
+        return f"{datetime_tuple[0]:04d}-{datetime_tuple[1]:02d}-{datetime_tuple[2]:02d}"
+
+    def get_formatted_time(self):
+        datetime_tuple = self.rtc.datetime()
+        formatted_time = f"{datetime_tuple[4]:02d}:{datetime_tuple[5]:02d}:{datetime_tuple[6]:02d}"
+        return formatted_time
+
     def get_datetime(self):
         return self.rtc.datetime()
